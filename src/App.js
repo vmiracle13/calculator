b8575dccefc8174cbdb8cb35b616ded7a20551f7
ViktoriyaB <vmiracle13@gmail.com>
2022-11-22 11:37:23 +0200

    create-calculator: add summing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [a, setA] = useState();
+  const [b, setB] = useState();
+  const [result, setResult] = useState();
+
+  const setNumber = (number) => {
+    if (a) {
+      setB(number);
+    } else {
+      setA(number);
+    }
+  };
+  const calculate = () => {
+    setResult(a + b);
+    setA();
+    setB();
+  };
+  const reset = () => {
+    setA(); setB(); setResult();
+  };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +39,21 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <main>
+        {[...Array(9).keys()].map((number) => (
+          <button onClick={() => setNumber(number)} key={number}>{number}</button>
+        ))}
+
+        <div>
+          <button type="button">+</button>
+          <button type="button" onClick={calculate}>=</button>
+        </div>
+
+        <p style={{ fontSize: 30, color: "green" }}>result = {result}</p>
+
+        <button type="button" onClick={reset}>Reset</button>
+      </main>
     </div>
   );
 }
